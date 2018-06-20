afb378451bb27b9c41ccc52a99fa865b6e6c7883
ritulit <orit.lieber@gmail.com>
2018-06-21 00:02:00 +0300

    DB interface update

--- inc/Database.php
+++ inc/Database.php
@@ -2,7 +2,15 @@
 Interface database{
 
   public function insertToDB($dest , $content);
+
+  public function deleteFromDB($source, $condition);
+
+  public function editFromDB($source, $data, $condition);
+
 }
+
+
+
 
 
 
